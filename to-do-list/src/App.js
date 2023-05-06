c001ac40aed498cd461a4832da89bb93e9c86641
Alberto <betim009@gmail.com>
2023-05-05 23:20:03 -0300

    Primeiro passo - criando as tasks - via props

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
 import './App.css';
 
+// Component Lista de Tarefas
+function ListaDeTarefas(props) {
+  const tasks = props.tasks;
+  const listaDeTarefas = tasks.map((task) => (
+    <li key={task.id}>{task.texto}</li>
+  ));
+  return <ul>{listaDeTarefas}</ul>;
+}
+
+
 function App() {
+  // Array de tasks
+  const tasks = [
+    { id: 1, texto: "HTML" },
+    { id: 2, texto: "CSS" },
+    { id: 3, texto: "React" },
+  ];
+
   return (
-    <div>Hello Word</div>
+    <div className="App">
+      <h1>Tasks de programação</h1>
+      <ListaDeTarefas tasks={tasks} />
+    </div>
   );
 }
 
